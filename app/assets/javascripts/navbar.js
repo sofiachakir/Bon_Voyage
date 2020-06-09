ca1b4e3dad1d638f6412873d1c6bcd866320cccf
delmasdev <delmas.dev@gmail.com>
2020-06-09 18:38:16 +0200

    add navbar

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,13 @@
+$(window).scroll(function(){
+  var scroll = $(window).scrollTop();
+    if(scroll < 600){
+      $('.navbar').addClass('navbar-blur')
+      $('.navbar').removeClass('navbar-black')
+      $('.navbar a').removeClass('navbartext')
+    }
+    else{
+      $('.navbar').removeClass('navbar-blur')
+      $('.navbar').addClass('navbar-black')
+      $('.navbar a').addClass('navbartext')
+    }
+});
