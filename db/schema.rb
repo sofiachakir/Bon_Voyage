7978471872a3fcd79976b7074ff3e7a8a77fd0d3
Eva Afriana <vava.afriana@gmail.com>
2020-06-09 15:04:11 +0200

    create table events

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_083158) do
+ActiveRecord::Schema.define(version: 2020_06_09_124014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name_event"
+    t.string "city_name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.text "comment"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "trip_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trip_id"], name: "index_events_on_trip_id"
+  end
 
   create_table "trips", force: :cascade do |t|
     t.string "title"
