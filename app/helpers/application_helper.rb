ffa678fc60eabc885736b0d0ff6b9f6fe70a9273
AtheDev <athe.dev@gmail.com>
2020-06-10 12:01:34 +0200

    add controller and views Event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,8 +11,21 @@ module ApplicationHelper
 
 	def is_trip_creator?
 		# si le current user est le trip.user il a accès, sinon redirigé
+		if params[:id] == nil
+		@trip = Trip.find(params[:trip_id])
+	else
 		@trip = Trip.find(params[:id])
+	end
 		unless current_user == @trip.user
+			flash[:error] = "Seul le créateur du voyage a accès à cette page"
+			redirect_to root_path
+		end
+	end
+
+	def is_event_creator?
+		# si le current user est le créateur de l'event il a accès, sinon redirigé
+		@event = Event.find(params[:id])
+		unless current_user == @event.trip.user
 			flash[:error] = "Seul le créateur du voyage a accès à cette page"
 			redirect_to root_path
 		end
