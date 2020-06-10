ed5721a0357fb9d8532b32256d7b8af9b57f25d1
sofiachakir <sofiachakir@gmail.com>
2020-06-10 10:33:24 +0200

    create user profile page, with upcoming trip, previous trips

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
       resources :notes
     end
   end
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
