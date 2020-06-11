094cb186674c858f96664d1235ecfbd9e3b22a17
sofiachakir <62932536+sofiachakir@users.noreply.github.com>
2020-06-11 19:18:17 +0200

    Revert "Dd active storage"

--- app/controllers/avatars_controller.rb
+++
@@ -1,7 +0,0 @@
-class AvatarsController < ApplicationController
-  def create
-    @user = User.find(params[:user_id])
-    @user.avatar.attach(params[:avatar])
-    redirect_to(user_path(@user))
-  end
-end
