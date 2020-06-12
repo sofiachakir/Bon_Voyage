0091e79d08fb6c73e85ef2bbcac07ea5fe2033af
AtheDev <athe.dev@gmail.com>
2020-06-12 14:02:00 +0200

    change button event#new day

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,20 +15,52 @@ class EventsController < ApplicationController
 
   def new
     @event = Event.new
+    @day = params[:format]
+    # params[:format] = Date.parse(@day.to_s)
+    # params[:format] = params[:format].to_time
+    puts "="*60
+    puts params
   end
 
   def create
-    @event = Event.new(event_params)
-    @trip = Trip.find(params[:trip_id])
-    @event.trip = @trip
+    # @event = Event.new(event_params)
+    # @p = params[:start_time]
+    @day = params[:format].to_time
+    # params[:format] = Date.parse(@day.to_s)
+    st = params[:start_time].to_time
+    et = params[:end_time].to_time
+    puts "="*60
+    puts "="*60
+    puts st
+    puts st.class
+    puts "="*60
+    puts et
+    puts et.class
+    puts "="*60
+    puts @day
+    puts @day.class
+    # params[:start_time] = DateTime.parse(params[:start_time] + @day)
+    # params[:end_time] = DateTime.parse(params[:end_time] + @day)
+    # u = params[:start_time].to_time
+    # st = u.hour + params[:format].to_time.hour
+    # @event = Event.new(city_name: params[:city_name],
+    #                     name_event: params[:name_event],
+    #                     start_time: params[:start_time],
+    #                     end_time: params[:end_time],
+    #                     comment: params[:comment],)
+    # @trip = Trip.find(params[:trip_id])
+    # @event.trip = @trip
+    # puts "="*60
+    # puts event_params
+    # puts params
 
-    if @event.save
-      flash[:success] = "Votre évènement a été créé"
-      redirect_to trip_events_path(@trip)
-    else
-      flash[:error] = @event.errors.full_messages
-      render :new
-    end
+    # if @event.save
+    #   flash[:success] = "Votre évènement a été créé"
+    #   redirect_to trip_events_path(@trip)
+    # else
+    #   flash[:error] = @event.errors.full_messages
+    #   render :new
+    # end
 
   end
 
@@ -55,6 +87,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:city_name, :name_event, :start_time, :end_time, :comment)
+    params.permit(:city_name, :name_event, :start_time, :end_time, :comment)
   end
 end
