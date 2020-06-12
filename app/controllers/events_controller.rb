ce5f6d3a4f2d8bc54d6ad33de301458a0f6653ec
sofiachakir <sofiachakir@gmail.com>
2020-06-12 15:43:29 +0200

    update event edit and new for start_time and end_time

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,65 +16,39 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
     @day = params[:format]
-    # params[:format] = Date.parse(@day.to_s)
-    # params[:format] = params[:format].to_time
-    puts "="*60
-    puts params
   end
 
   def create
-    # @event = Event.new(event_params)
-    # @p = params[:start_time]
-    @day = params[:format].to_time
-    # params[:format] = Date.parse(@day.to_s)
-    st = params[:start_time].to_time
-    et = params[:end_time].to_time
-    puts "="*60
-    puts "="*60
-    puts st
-    puts st.class
-    puts "="*60
-    puts et
-    puts et.class
-    puts "="*60
-    puts @day
-    puts @day.class
-    # params[:start_time] = DateTime.parse(params[:start_time] + @day)
-    # params[:end_time] = DateTime.parse(params[:end_time] + @day)
-    # u = params[:start_time].to_time
-    # st = u.hour + params[:format].to_time.hour
-    # @event = Event.new(city_name: params[:city_name],
-    #                     name_event: params[:name_event],
-    #                     start_time: params[:start_time],
-    #                     end_time: params[:end_time],
-    #                     comment: params[:comment],)
-    # @trip = Trip.find(params[:trip_id])
-    # @event.trip = @trip
-    # puts "="*60
-    # puts event_params
-    # puts params
-
-    # if @event.save
-    #   flash[:success] = "Votre évènement a été créé"
-    #   redirect_to trip_events_path(@trip)
-    # else
-    #   flash[:error] = @event.errors.full_messages
-    #   render :new
-    # end
+    @event = Event.new(event_params)
+    @trip = Trip.find(params[:trip_id])
+    @event.trip = @trip
+    
+    if @event.save
+      flash[:success] = "Votre évènement a été créé"
+      redirect_to trip_events_path(@trip)
+    else
+      flash[:error] = @event.errors.full_messages
+      render :new
+    end
 
   end
 
   def edit
     @event = Event.find(params[:id])
     @trip = Trip.find(params[:trip_id])
+    @day = params[:format]
   end
 
   def update
     @trip = Trip.find(params[:trip_id])
     @event = Event.find(params[:id])
-    @event.update(event_params)
-    flash[:success] = "Votre évènement a été mis à jour"
-    redirect_to trip_events_path(@trip)
+    if @event.update(event_params)
+      flash[:success] = "Votre évènement a été mis à jour"
+      redirect_to trip_events_path(@trip)
+    else
+      flash[:error] = @event.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
@@ -87,6 +61,25 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.permit(:city_name, :name_event, :start_time, :end_time, :comment)
+    event_params = Hash.new
+    event_params = { city_name: params[:city_name],
+                     name_event: params[:name_event],
+                     start_time: new_start_time,
+                     end_time: new_end_time,
+                     comment: params[:comment] }
+  end
+
+  def new_start_time
+    day = Time.parse(params[:format])
+    st_hour = params[:start_time]['(4i)'].to_i
+    st_min = params[:start_time]['(5i)'].to_i
+    start_time = day.change(hour: st_hour, min: st_min)
+  end
+
+  def new_end_time
+    day = Time.parse(params[:format])
+    et_hour = params[:end_time]['(4i)'].to_i
+    et_min = params[:end_time]['(5i)'].to_i
+    end_time = day.change(hour: et_hour, min: et_min)
   end
 end
