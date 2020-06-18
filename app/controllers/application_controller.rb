3b42d293dde1945fc7ceb187aaaa5399d600d09e
EmilieDufau <dufau.emilie@gmail.com>
2020-06-18 12:16:37 +0200

    redirection sign-up to profil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
 	include ApplicationHelper
+
+	def after_sign_in_path_for(resource)
+		flash[:notice] = "Veuillez compléter votre profil"
+		user_path(current_user) unless current_user.completed_profile?
+	end
+
 end
